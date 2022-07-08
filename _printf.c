b40503db77d1b599b4b3570761f44b88c7ee81bd
Ivan Hansen <ihk225@gmail.com>
2022-07-08 11:44:30 -0300

    A

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -14,8 +13,7 @@ int _printf(const char *format, ...)
     int i = 0, j = 0;
     ftype func_data;
     va_list ap;
-
-    printf("Test1");
+    
     va_start(ap, format);
         while ((format[i] != '\0') && (format[i]))
         {
