6727e053eefc915d322612d8f8f409907ac7e0ee
Ivan Hansen <ihk225@gmail.com>
2022-07-08 12:14:33 -0300

    Test commit

--- _printf.c
+++ _printf.c
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
                         i = ((*(func_data.fmt) == '.') ? (i + 4) : (i + 2));
                     }
                 }
-                else if ((format[i] == '%') && (format[i + 1] == '%') || (format[i + 1] == '\0')))
+                else if ((format[i] == '%') && ((format[i + 1] == '%') || (format[i + 1] == '\0')))
                 {
                     i++;
                     j += singlewrite(format[i++]);
