c248740fb9d178ae16b7af5dd987c22bb0df6c37
Ivan Hansen <ihk225@gmail.com>
2022-07-08 12:08:54 -0300

    Test commit

--- _printf.c
+++ _printf.c
@@ -19,28 +19,31 @@ int _printf(const char *format, ...)
     {
         return (0);
     }
-    va_start(ap, format);
-        while ((format[i] != '\0') && (format != NULL))
-        {
-            if ((format[i] == '%') && (format[i + 1] != '%'))
-			{
-                func_data = func_finder(format[i + 1]);
-			    if (func_data.op != 0)
+    else
+    {
+        va_start(ap, format);
+            while ((format[i] != '\0') && (format != NULL))
+            {
+                if ((format[i] == '%') && (format[i + 1] != '%'))
                 {
-                    j += (*(func_data.op))(ap, func_data.mode);
-			        i = ((*(func_data.fmt) == '.') ? (i + 4) : (i + 2));
+                    func_data = func_finder(format[i + 1]);
+                    if (func_data.op != 0)
+                    {
+                        j += (*(func_data.op))(ap, func_data.mode);
+                        i = ((*(func_data.fmt) == '.') ? (i + 4) : (i + 2));
+                    }
+                }
+                else if ((format[i] == '%') && (format[i + 1] == '%'))
+                {
+                    i++;
+                    j += singlewrite(format[i++]);
+                }
+                else
+                {
+                    j += singlewrite(format[i++]);
                 }
             }
-            else if ((format[i] == '%') && (format[i + 1] == '%'))
-            {
-                i++;
-                j += singlewrite(format[i++]);
-            }
-            else
-            {
-                j += singlewrite(format[i++]);
-            }
-        }
     va_end(ap);
+    }
     return (j);
 }
