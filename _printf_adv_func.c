22002fc018d0d9e50bb442178107a8beaffd405c
Nanxuss <66189578+Nanxuss@users.noreply.github.com>
2022-07-11 18:49:06 -0300

    Update _printf_adv_func.c

--- _printf_adv_func.c
+++ _printf_adv_func.c
@@ -53,11 +53,6 @@ char *rot13(char *s)
  * _strdup - copy of the string given as a parameter
  * @str: string of char
  *
- * Description: The _strdup() function returns a pointer to a new string which
- * is a duplicate of the string str. Memory for the new string is
- * obtained with malloc, and can be freed with free.
- * Returns NULL if str = NULL
- *
  * Return: a pointer to the duplicated string. It returns
  * NULL if insufficient memory was available
  */
